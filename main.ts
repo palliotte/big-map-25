6896a8d3934d41fd4dc6216fb9b4eec39d538240
palliotte <114391946+palliotte@users.noreply.github.com>
2023-02-13 18:39:21 +0800

    Update main.blocks, main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,37 +1,39 @@
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
     
-    if (!(playery > 23)) {
+    if (!(playery > 24)) {
         playery += 1
     }
     
 })
 input.onGesture(Gesture.TiltLeft, function on_gesture_tilt_left() {
     
-    if (!(playerx > 3)) {
+    if (!(playerx > 0)) {
         playerx += -1
     }
     
 })
 input.onButtonPressed(Button.B, function on_button_pressed_b() {
     
-    if (!(playery > 3)) {
+    if (!(playery > 0)) {
         playery += -1
     }
     
 })
 input.onGesture(Gesture.TiltRight, function on_gesture_tilt_right() {
     
-    if (!(playerx > 23)) {
+    if (!(playerx > 24)) {
         playerx += 1
     }
     
 })
-let playerx = 0
 let playery = 0
+let playerx = 0
+let obstacless : number[] = []
+playerx = 0
+playery = 0
 let player = game.createSprite(2, 2)
 let obstacles = [1, 0, 1, 1, 2, 1, 3, 1, 4, 1]
-forever(function on_forever() {
-    for (let n = 0; n < obstacles.length - 1; n++) {
-        
-    }
-})
+function show_obstacles() {
+    
+}
+
