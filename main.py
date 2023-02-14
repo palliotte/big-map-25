505625c9f029cbf3ddb2da876b6491ffc570bd25
palliotte <114391946+palliotte@users.noreply.github.com>
2023-02-14 11:46:42 +0800

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,33 +1,45 @@
+def show_obstacles():
+    for n in range(1, len(obstacles)):
+        if 0 > playerx - obstacles[n * 2 - 1] < 2 and playery - obstacles[n * 2] < 2:
+            obstacless.append(game.create_sprite(playerx - obstacles[n * 2 - 1], playery - obstacles[n * 2]))
+        if 0 > obstacles[n * 2 - 1] - playerx < 2 and obstacles[n * 2] - playery < 2:
+            obstacless.append(game.create_sprite(obstacles[n * 2 - 1] - playerx, obstacles[n * 2] - playery))
+        if 0 > obstacles[n * 2 - 1] - playerx < 2 and playery - obstacles[n * 2] < 2:
+            obstacless.append(game.create_sprite(obstacles[n * 2 - 1] - playerx, playery - obstacles[n * 2]))
+        if 0 > playerx - obstacles[n * 2 - 1] < 2 and obstacles[n * 2] - playery < 2:
+            obstacless.append(game.create_sprite(playerx - obstacles[n * 2 - 1], obstacles[n * 2] - playery))
+
 def on_button_pressed_a():
     global playery
     if not (playery > 24):
         playery += 1
+        show_obstacles()
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_gesture_tilt_left():
     global playerx
     if not (playerx > 0):
         playerx += -1
+        show_obstacles()
 input.on_gesture(Gesture.TILT_LEFT, on_gesture_tilt_left)
 
 def on_button_pressed_b():
     global playery
     if not (playery > 0):
         playery += -1
+        show_obstacles()
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def on_gesture_tilt_right():
     global playerx
     if not (playerx > 24):
         playerx += 1
+        show_obstacles()
 input.on_gesture(Gesture.TILT_RIGHT, on_gesture_tilt_right)
 
-obstacless: List[number] = []
-playerx = 0
 playery = 0
+playerx = 0
+obstacless: List[game.LedSprite] = []
 player = game.create_sprite(2, 2)
 obstacles = [1, 0, 1, 1, 2, 1, 3, 1, 4, 1]
-
-def show_obstacles():
-    for n in range(1,obstacles.length):
-        if playerx-obstacles[n*2-&]
+show_obstacles()
--- main.ts
+++ main.ts
@@ -1,7 +1,29 @@
+function show_obstacles() {
+    for (let n = 1; n < obstacles.length; n++) {
+        if (0 > playerx - obstacles[n * 2 - 1] && playerx - obstacles[n * 2 - 1] < 2 && playery - obstacles[n * 2] < 2) {
+            obstacless.push(game.createSprite(playerx - obstacles[n * 2 - 1], playery - obstacles[n * 2]))
+        }
+        
+        if (0 > obstacles[n * 2 - 1] - playerx && obstacles[n * 2 - 1] - playerx < 2 && obstacles[n * 2] - playery < 2) {
+            obstacless.push(game.createSprite(obstacles[n * 2 - 1] - playerx, obstacles[n * 2] - playery))
+        }
+        
+        if (0 > obstacles[n * 2 - 1] - playerx && obstacles[n * 2 - 1] - playerx < 2 && playery - obstacles[n * 2] < 2) {
+            obstacless.push(game.createSprite(obstacles[n * 2 - 1] - playerx, playery - obstacles[n * 2]))
+        }
+        
+        if (0 > playerx - obstacles[n * 2 - 1] && playerx - obstacles[n * 2 - 1] < 2 && obstacles[n * 2] - playery < 2) {
+            obstacless.push(game.createSprite(playerx - obstacles[n * 2 - 1], obstacles[n * 2] - playery))
+        }
+        
+    }
+}
+
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
     
     if (!(playery > 24)) {
         playery += 1
+        show_obstacles()
     }
     
 })
@@ -9,6 +31,7 @@ input.onGesture(Gesture.TiltLeft, function on_gesture_tilt_left() {
     
     if (!(playerx > 0)) {
         playerx += -1
+        show_obstacles()
     }
     
 })
@@ -16,6 +39,7 @@ input.onButtonPressed(Button.B, function on_button_pressed_b() {
     
     if (!(playery > 0)) {
         playery += -1
+        show_obstacles()
     }
     
 })
@@ -23,17 +47,13 @@ input.onGesture(Gesture.TiltRight, function on_gesture_tilt_right() {
     
     if (!(playerx > 24)) {
         playerx += 1
+        show_obstacles()
     }
     
 })
 let playery = 0
 let playerx = 0
-let obstacless : number[] = []
-playerx = 0
-playery = 0
+let obstacless : game.LedSprite[] = []
 let player = game.createSprite(2, 2)
 let obstacles = [1, 0, 1, 1, 2, 1, 3, 1, 4, 1]
-function show_obstacles() {
-    
-}
-
+show_obstacles()
